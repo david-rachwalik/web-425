0f7f919eeb368a19e9ee318e41c7144bd6c9b509
David Rachwalik <david.rachwalik@outlook.com>
2022-06-11 14:29:16 -0500

    modified composer-list component for external data

--- week-03/enhanced-composer-app/src/app/composer-list/composer-list.component.ts
+++ week-03/enhanced-composer-app/src/app/composer-list/composer-list.component.ts
@@ -1,21 +1,13 @@
 /*
  * Title: composer-list.component.ts
  * Author: David Rachwalik
- * Date: 2022/06/05
+ * Date: 2022/06/11
  * Description: Composer list component
  */
 
 import { Component, OnInit } from '@angular/core';
-
-export default class Composer {
-  fullName: string;
-  genre: string;
-
-  constructor(fullName: string, genre: string) {
-    this.fullName = fullName;
-    this.genre = genre;
-  }
-}
+import { Composer } from '../composer.class';
+import { IComposer } from '../composer.interface';
 
 @Component({
   selector: 'app-composer-list',
@@ -23,16 +15,10 @@ export default class Composer {
   styleUrls: ['./composer-list.component.scss'],
 })
 export class ComposerListComponent implements OnInit {
-  composers: Array<Composer>;
+  composers: Array<IComposer>;
 
   constructor() {
-    this.composers = [
-      new Composer('Ludwig van Beethoven', 'Classical'),
-      new Composer('Johann Sebastian Bach', 'Classical'),
-      new Composer('Wolfgang Amadeus Mozart', 'Classical'),
-      new Composer('Johannes Brahms', 'Classical'),
-      new Composer('Joseph Haydn', 'Classical'),
-    ];
+    this.composers = new Composer().getComposers();
   }
 
   ngOnInit(): void {}
