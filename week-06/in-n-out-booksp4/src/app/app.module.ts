5d10ce7a632fd257459ec8304d6c61ce0c3ab6ca
David Rachwalik <david.rachwalik@outlook.com>
2022-07-03 21:18:39 -0500

    exercise 6.2 initial commit

---
+++ week-06/in-n-out-booksp4/src/app/app.module.ts
@@ -0,0 +1,52 @@
+/*
+ * Title: app.module.ts
+ * Author: David Rachwalik
+ * Date: 2022/06/26
+ * Description: App module
+ */
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookDetailsDialogComponent } from './book-details-dialog/book-details-dialog.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    BookListComponent,
+    BookDetailsDialogComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatTableModule,
+    MatDialogModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
