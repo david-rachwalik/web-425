721fcdaf3e991a62b9efb0442ce643673faf1cef
David Rachwalik <david.rachwalik@outlook.com>
2022-07-03 22:05:27 -0500

    exercise 6.3 components added

--- week-06/gpa-calculator-app1/src/app/app.module.ts
+++ week-06/gpa-calculator-app1/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthLayoutComponent,
+    BaseLayoutComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
